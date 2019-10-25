8fa75ee8b9c1281986a0bd95214d7549e4e7b5e7
Deepak Sharma <Deepak.Sharma@global.internal>
2019-10-25 17:24:37 +0100

    added a button

--- app/src/main/java/hk/ust/cse/comp107x/greetfriend/MainActivity.java
+++ app/src/main/java/hk/ust/cse/comp107x/greetfriend/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private static final String TAG = "MainActivity";
     private Button greetButton;
+    private Button timeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
